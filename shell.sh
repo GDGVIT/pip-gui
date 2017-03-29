a69280bd35cb1df9df115cd41463face3f34bcfa
Ayush Priya <ayushpriya10@gmail.com>
2017-03-29 16:02:12 +0530

    Finishing UI

---
+++ shell.sh
@@ -0,0 +1,16 @@
+git filter-branch --env-filter '
+WRONG_EMAIL="root@localhost.localdomain"
+NEW_NAME="Ayush Priya"
+NEW_EMAIL="ayushpriya10@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
