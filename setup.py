89c1fa97218a2e3cf086938dab5956b84b53de11
Paulo Jorge <86368444+teckscam@users.noreply.github.com>
2021-07-01 18:43:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,8 +35,7 @@ setup(
         "pipgui",
         "pipgui.*"
     ]),
-    data_files=[('pipgui/Resource_Files', [f for f in copy_dir()]
-                )],
+    data_files=[('pipgui/Resource_Files', [f for f in copy_dir()])],
     python_requires='>=3.4',
     include_package_data=True,
     entry_points={
